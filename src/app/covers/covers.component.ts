9e8117cd006b3a4e68c7f2673b8f48aaa5926bc2
vlatko.miloshevski@gmail.com <Vlatko.Miloshevski@endava.com>
2019-01-09 12:53:45 +0100

    css (flexbox layout) adjustments

--- src/app/covers/covers.component.ts
+++ src/app/covers/covers.component.ts
@@ -50,11 +50,11 @@ export class CoversComponent implements OnInit, OnChanges {
   }
 
   handleImagesSize(covers) {
-    this.imageWidth = "150";
+    this.imageWidth = "180";
     this.checked = false;
 
     if (covers) {
-      this.imageWidth = covers.showLargeImages ? "200" : "150";
+      this.imageWidth = covers.showLargeImages ? "230" : "180";
       this.checked = covers.showLargeImages ? true : false;
     }
   }
